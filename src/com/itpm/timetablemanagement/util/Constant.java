eae2c68c07d66e65c49e1c959858ef590c153663
Hasini <hasinidilanka2266@gmail.com>
2021-05-21 21:28:15 +0530

    main: member2

---
+++ src/com/itpm/timetablemanagement/util/Constant.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itpm.timetablemanagement.util;
+
+/**
+ *
+ * @author gayan
+ */
+public class Constant {
+    
+    public static final String CREATE_DATABASE = "create_database";
+    
+   
+    public static final String INSERT_WORKING_HOUR_TABLE = "Insert_workingdays_and_hours";
+    public static final String INSERT_WE_WORKING_HOUR_DAYS_TABLE = "Insert_WE_workingdays_and_hours_days";
+    public static final String INSERT_WD_WORKING_HOUR_DAYS_TABLE = "Insert_WD_workingdays_and_hours_days";
+    public static final String REMOVE_WORKING_HOUR_TABLE = "Remove_workingdays_and_hours";
+    public static final String REMOVE_WORKING_HOUR_DAYS_TABLE = "Remove_workingdays_and_hours_days";
+    public static final String GET_WORKING_HOUR_TABLE = "Get_workingdays_and_hours";
+    public static final String GET_WORKING_HOUR_TABLE_BY_TYPE = "Get_workingdays_and_hours_by_type";
+    
+    public static final String INSERT_LECTURER = "insert_lecturer";
+    public static final String GET_LECTURER = "get_lecturer";
+    public static final String UPDATE_LECTURER = "update_lecturer";
+    public static final String DELETE_LECTURER= "delete_lecturer";
+    
+    public static final String INSERT_SUBJECTS = "insert_subject";
+    public static final String UPDATE_SUBJECT = "update_subject";
+    public static final String DELETE_SUBJECTS= "delete_subject";
+    public static final String GET_SUBJECTS = "get_subjects";
+    
+    public static final String INSERT_SESSION = "insert_session";
+    public static final String GET_SESSION = "get_session";
+    public static final String DELETE_SESSION= "delete_session";
+    public static final String UPDATE_SESSION = "update_session";
+    
+    public static final String QUERY = "query";
+    public static final String QUERY_ID = "id";
+    public static final String PROPERTIES = "timetablemanagement.properties";
+    public static final String DB_PASSWORD = "password";
+    public static final String DB_USERNAME = "username";
+    public static final String DB_URL = "url";
+    public static final String DB_DRIVER = "driver";
+    
+    
+}
